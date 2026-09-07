ebc9f45b0823a185d3481c5b081203775e4d8bac
Sain-Biswas <sain.biswas614@gmail.com>
2026-09-07 21:24:36 +0530

    feat: setup a basic tanstack start application

---
+++ src/routes/__root.tsx
@@ -0,0 +1,50 @@
+import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
+import globalStyles from "~/styles/styles.css?url";
+
+interface AnivaRouterContext {}
+
+export const Route = createRootRouteWithContext<AnivaRouterContext>()({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8"
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1"
+			},
+			{
+				title: "Aniva"
+			}
+		],
+
+		links: [
+			{
+				rel: "stylesheet",
+				href: globalStyles
+			}
+		]
+	}),
+
+	shellComponent: ({ children }: { children: React.ReactNode }) => {
+		return (
+			<html lang="en">
+				<head>
+					<HeadContent />
+				</head>
+				<body>
+					{children}
+					<Scripts />
+				</body>
+			</html>
+		);
+	},
+
+	notFoundComponent: () => {
+		return (
+			<main>
+				<h1>Page not found</h1>
+			</main>
+		);
+	}
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { nitro } from "nitro/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		devtools(),
+		nitro(),
+		tanstackStart(),
+		react(),
+		babel({ presets: [reactCompilerPreset({ target: "19" })] })
+	],
+
 	resolve: {
 		tsconfigPaths: true
 	}
